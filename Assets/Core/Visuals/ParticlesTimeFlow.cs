31c9bed437cef7c222467888f25bb81130865b0a
artak10t <artak10t@gmail.com>
2019-03-17 16:20:25 -0700

    ProceduralSpace-source

---
+++ Assets/Core/Visuals/ParticlesTimeFlow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable 0618
+
+public class ParticlesTimeFlow : MonoBehaviour {
+
+    //Warning, playbackSpeed is obsolete
+    //Gets all childrens' particles and changes simulation speed
+
+    private ParticleSystem[] particles;
+
+    private void Start()
+    {
+        particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update () {
+        if (particles[0].playbackSpeed != GlobalSettings.singleton.TimeFlow)
+        {
+            for (int i = 0; i < particles.Length; i++)
+            {
+                particles[i].playbackSpeed = Mathf.Abs(GlobalSettings.singleton.TimeFlow);
+            }
+        }
+	}
+}
